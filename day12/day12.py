57c06d2f8b17e5b215f01c8379542b44f6ba9e4c
churrowaffles <minhui00@gmail.com>
2023-01-25 11:13:15 +0800

    day12 part 2 solve

--- day12/day12.py
+++ day12/day12.py
@@ -1,10 +1,13 @@
-def start_coordinate(text_file, heightmap):
-    i, j, x, y = 0, 0, 0, 0
+def start_and_end(text_file):
+    i, j = 0, 0
 
     with open(text_file) as file:
         for line in file.read():
             if line == 'S':
                 x, y = i, j
+
+            if line == 'E':
+                x2, y2 = i, j
 
             if line == '\n':
                 j += 1
@@ -14,9 +17,9 @@ def start_coordinate(text_file, heightmap):
                 i += 1
 
     heightmap[(x, y)] = 'a'
-    return (x,y)
+    return ((x,y),(x2,y2))
 
-def valid_coordinate(heightmap, current_c, compare_c):
+def valid_coordinate(current_c, compare_c):
     if not heightmap.get(compare_c):
         return False
     
@@ -31,7 +34,20 @@ def valid_coordinate(heightmap, current_c, compare_c):
         return True
     return False
 
-def BFS(heightmap, distance, visited_locations, locations_to_visit):
+def valid_coordinate_reverse(current_c, compare_c):
+    if not heightmap.get(compare_c):
+        return False
+    
+    current = ord(heightmap[current_c])
+    compare = ord(heightmap[compare_c])
+
+    if compare > current - 2:
+        return True
+    return False
+
+def BFS(locations_to_visit, f, endpoint):
+    distance = 0
+    visited_locations = {}
     while locations_to_visit:
         location = locations_to_visit[0]
 
@@ -40,16 +56,15 @@ def BFS(heightmap, distance, visited_locations, locations_to_visit):
             locations_to_visit.append(None)
 
         elif not visited_locations.get(location):
-            if heightmap[location] == 'E':
-                return distance
-
             left = (location[0] - 1, location[1])
             right = (location[0] + 1, location[1])
             top = (location[0], location[1] - 1)
             bottom = (location[0], location[1] + 1)
 
             for c in [left, right, top, bottom]:
-                if valid_coordinate(heightmap, location, c):
+                if f(location, c):
+                    if heightmap[c] == endpoint:
+                        return distance + 1
                     locations_to_visit.append(c)
 
             visited_locations[location] = distance
@@ -59,7 +74,17 @@ def BFS(heightmap, distance, visited_locations, locations_to_visit):
 
 # PART ONE SOLUTION
 heightmap = {}
-starting_point = start_coordinate('day12.txt', heightmap)
+start_end_coordinates = start_and_end('day12.txt')
+starting_point, ending_point = start_end_coordinates[0], start_end_coordinates[1]
+
+# Start from the starting point and navigate to 'E'
 locations_to_visit = [starting_point, None]
-shortest_path = BFS(heightmap, distance=0, visited_locations={}, locations_to_visit=locations_to_visit)
-print(shortest_path)
+shortest_path = BFS(locations_to_visit=locations_to_visit, f=valid_coordinate, endpoint='E')
+print("Part One: The shortest path will take", shortest_path, "steps.")
+
+# PART TWO SOLUTION
+
+# Start from the 'E' and navigate to the nearest 'a'
+locations_to_visit = [ending_point, None]
+shortest_path = BFS(locations_to_visit=locations_to_visit, f=valid_coordinate_reverse, endpoint='a')
+print("Part Two: The shortest path will take", shortest_path, "steps.")
