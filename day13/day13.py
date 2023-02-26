6a3f06509f1b36c0b353e38db5b54eabebb8b98a
churrowaffles <106877391+churrowaffles@users.noreply.github.com>
2023-02-26 08:15:11 +0000

    day 13 part 2

--- day13/day13.py
+++ day13/day13.py
@@ -1,6 +1,8 @@
 from ast import literal_eval
 
+
 def main(file):
+    # PART ONE
     signal = parse_file(file)
     sum_of_indices = 0
 
@@ -10,9 +12,22 @@ def main(file):
             sum_of_indices += index + 1
     print("Part 1:", sum_of_indices)
 
+    # PART TWO
+    signal = parse_file_p2(file)
+    additional = [[[2]], [[6]]]
+    signal.extend(additional)
+
+    # Sort all the packets and look for target
+    sorted_signals = merge_sort(signal)
+    decoder = 1
+    for index, packet in enumerate(sorted_signals):
+        if packet in additional:
+            decoder *= index + 1
+    print("Part 2:", decoder)
+    
 
 def parse_file(text):
-    # Parse input into an ordered list of packets pair
+    # Parse input into an ordered list of packets pairs
     # Each element of the list should contain 2 list (both packets) - first/left/A + second/right/B
     with open(text, 'r') as file:
         signal = file.read().split('\n\n')
@@ -25,6 +40,12 @@ def parse_file(text):
     return signal
 
 
+def parse_file_p2(text):
+    # Parse input into individual lists of packets
+    with open(text, 'r') as file:
+        return [literal_eval(line.rstrip()) for line in file if line != '\n']
+
+
 def compare_elements(a, b):
     '''
     If both are integers, if A < B = True || A > B = False; continue comparison if values are the same
@@ -33,7 +54,6 @@ def compare_elements(a, b):
       - If List A runs out before conclusion, it is in the right order
       - If List B runs out before conclusion, it is in the wrong order
     '''
-    
     i = 0
     while i < max(len(a), len(b)):
         try:
@@ -46,12 +66,12 @@ def compare_elements(a, b):
                 continue
             
             if isinstance(a[i], int):
-                a[i] = [a[i]]
-
+                check_inner_list = compare_elements([a[i]], b[i])
             elif isinstance(b[i], int):
-                b[i] = [b[i]]
-                
-            check_inner_list = compare_elements(a[i], b[i])
+                check_inner_list = compare_elements(a[i], [b[i]]) 
+            else:
+                check_inner_list = compare_elements(a[i], b[i])
+            
             if check_inner_list is not None:
                 return check_inner_list
             i += 1
@@ -59,6 +79,31 @@ def compare_elements(a, b):
         except IndexError:
             return len(a) < len(b)
     
+
+def merge_sort(signal):
+    length = len(signal)
+    if length == 1:
+        return signal
+    
+    left = merge_sort(signal[:length//2])
+    right = merge_sort(signal[length//2:])
+
+    i, j = 0, 0
+    merged = []
+    while i < len(left) and j < len(right):
+        if compare_elements(left[i], right[j]):
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    
+    if i < len(left):
+        merged.extend(left[i:])
+    elif j < len(right):
+        merged.extend(right[j:])
+    
+    return merged
 
 
 main('day13.txt')
